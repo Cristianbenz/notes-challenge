904576c42ca7d395b4e935e1fb0f5373a3eb2496
cbenz <contactocbenitez@gmail.com>
2023-09-03 16:32:13 -0300

    import home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+import { HomeComponent } from './pages/home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
