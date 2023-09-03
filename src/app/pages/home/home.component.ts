1283cad1e83a3745ccbba9cd5bec20d7c2c903b9
cbenz <contactocbenitez@gmail.com>
2023-09-03 17:51:36 -0300

    import notes list into home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AddNoteButtonComponent } from 'src/app/components/add-note-button/add-note-button.component';
+import { NotesListComponent } from 'src/app/components/notes-list/notes-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, AddNoteButtonComponent],
+  imports: [CommonModule, AddNoteButtonComponent, NotesListComponent],
   templateUrl: './home.component.html',
   styles: [
   ]
