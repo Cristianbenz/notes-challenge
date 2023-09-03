5e9b8ff17af70f88a80f7c4133f4c37b0db9c04d
cbenz <contactocbenitez@gmail.com>
2023-09-03 19:00:15 -0300

    add id creation

---
+++ src/app/components/todo-form/todo-form.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialogRef } from "@angular/material/dialog"
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
+import { ToDoService } from 'src/app/services/todo.service';
+
+@Component({
+  selector: 'app-todo-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatButtonModule],
+  templateUrl: './todo-form.component.html',
+  styleUrls: ['./todo-form.component.scss']
+})
+export class ToDoFormComponent {
+  private _dialog: MatDialogRef<ToDoFormComponent> = inject(MatDialogRef<ToDoFormComponent>)
+  private _formBuilder: FormBuilder = inject(FormBuilder);
+  private _toDosService: ToDoService = inject(ToDoService)
+
+  public createForm = this._formBuilder.group({
+    title: ["", Validators.required],
+    description: ["", Validators.required],
+  })
+
+  addNote() {
+    const todo = {
+      id: new Date().toLocaleString().replace(" ", ''),
+      title: String(this.createForm.value.title),
+      description: String(this.createForm.value.description)
+    }
+    if(todo.title && todo.description) {
+      this._toDosService.addNote(todo)
+      this._dialog.close()
+    }
+  }
+
+  closeForm() {
+    this._dialog.close()
+  }
+}
