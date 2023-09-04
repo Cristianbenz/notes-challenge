21aa840bc11cbba0a802c59c6db07ee6a7544b16
cbenz <contactocbenitez@gmail.com>
2023-09-04 10:57:45 -0300

    import nav bar into root layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 
 import { HomeComponent } from './pages/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, NavBarComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
