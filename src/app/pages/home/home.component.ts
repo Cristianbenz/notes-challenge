d0b9bca7e75601a4fcdf2b42f277a1feac540509
cbenz <contactocbenitez@gmail.com>
2023-09-04 11:08:33 -0300

    import auth form and show it if user is not autenticated

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthService } from 'src/app/services/auth.service';
 import { AddToDoButtonComponent } from 'src/app/components/add-todo-button/add-todo-button.component';
 import { ToDoListComponent } from 'src/app/components/todo-list/todo-list.component';
+import { UserFormComponent } from 'src/app/components/user-form/user-form.component';
+import { User } from 'src/app/models/user';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, AddToDoButtonComponent, ToDoListComponent],
+  imports: [CommonModule, AddToDoButtonComponent, ToDoListComponent, UserFormComponent],
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent {
+  private _authService: AuthService = inject(AuthService);
+  public user: User | null = null;
 
+  constructor() {
+    this._authService.user.subscribe(user => this.user = user);
+  }
+  
 }
